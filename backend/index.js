e91f75013673df830537f88cd7727e693386f7c3
ravikant <ravikant.dwivedi@rapidsoft.co.in>
2022-11-21 18:17:04 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express =require('express')
+require('dotenv').config()
+const app= express()
+
+const PORT =process.env.PORT
+app.listen(PORT,()=>{
+    console.log(`server is running at port ${PORT}`)
+})
